beeeefe7837c8f46e1f252b732414d171e76ce0b
Adam Wentz <adam.wentz@dollarshaveclub.com>
2016-12-05 22:03:35 -0600

    Stealthier location of random string

--- coolmit/cpu.py
+++ coolmit/cpu.py
@@ -10,10 +10,10 @@ from .base import CoolmitMiner
 
 def coolmit_probe_worker(args):
     """Coolmit worker which helps find that cool commit hash you crave."""
-    prefix, msg_template, charset, worker_id, num_workers, start_offset, probe_length, work_chunk_size = args
+    prefix, msg_header, msg_body, charset, worker_id, num_workers, start_offset, probe_length, work_chunk_size = args
     hf = hashlib.sha1
     len_charset = len(charset)
-    msg_h = hf(msg_template)
+    msg_h = hf(msg_header)
     for i in xrange(start_offset + worker_id, start_offset + work_chunk_size, num_workers):
         salt = ''
         remaining = i
@@ -22,6 +22,7 @@ def coolmit_probe_worker(args):
             remaining = remaining // len_charset
         h = msg_h.copy()
         h.update(salt)
+        h.update(msg_body)
         h = h.hexdigest()
         if h.startswith(prefix):
             return salt, h
@@ -36,21 +37,21 @@ class CoolmitCPUMiner(CoolmitMiner):
     def pre_mine(self):
         self.pool = Pool(self.num_workers)
 
-    def _probe(self, prefix, message, probe_length, chunk_size):
+    def _probe(self, prefix, header, body, probe_length, chunk_size):
         work_chunk_size = self.num_workers * chunk_size
         num_combinations = len(self.charset) ** probe_length
-        
+
         start_offset = 0
         start_t = time.time()
         while start_offset < num_combinations:
             params = [
-                (prefix, message, self.charset, worker_id, self.num_workers, start_offset, probe_length, work_chunk_size) \
+                (prefix, header, body, self.charset, worker_id, self.num_workers, start_offset, probe_length, work_chunk_size) \
                 for worker_id in range(self.num_workers)
             ]
             results = self.pool.map(coolmit_probe_worker, params)
             for result in results:
-                if result:       
-                    return result 
+                if result:
+                    return result
             start_offset += work_chunk_size
             if start_offset % (work_chunk_size * 1) == 0:
                 dt = time.time() - start_t
